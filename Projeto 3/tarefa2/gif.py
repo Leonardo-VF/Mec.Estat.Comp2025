20a98f308a0d2fe030bddc11d915a034cd78c2bc
Leonardo-VF <ferreira.leov@gmail.com>
2025-04-26 19:12:17 -0300

    leo

---
+++ Projeto 3/tarefa2/gif.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Parâmetros
+N = 150  # Tamanho da malha
+arquivo_saida = "saida_1_13862330.out"  # Nome do arquivo gerado no Fortran
+
+# Ler o arquivo
+with open(arquivo_saida, 'r') as f:
+    linhas = f.readlines()
+
+# Interpretar os frames
+frames = []
+frame = []
+
+for linha in linhas:
+    linha = linha.strip()
+    if linha == "":
+        continue  # Ignora linhas vazias (se tiver)
+
+    numeros = [int(linha[i:i+4]) for i in range(0, len(linha), 4)]
+    frame.append(numeros)
+
+    if len(frame) == N:
+        frames.append(np.array(frame))
+        frame = []
+
+# Configurar a figura
+fig, ax = plt.subplots()
+im = ax.imshow(frames[0], cmap='Greys', vmin=0, vmax=1)
+ax.axis('off')
+
+# Função de atualização
+def update(frame):
+    im.set_data(frame)
+    return [im]
+
+# Criar a animação
+ani = animation.FuncAnimation(fig, update, frames=frames, interval=200, blit=True)
+
+# Salvar o vídeo
+ani.save('evolucao_DLA.mp4', writer='ffmpeg', fps=60)
+
+print("Vídeo salvo como evolucao_DLA.mp4")
--- Projeto 3/tarefa2/tarefa2.f90
+++ Projeto 3/tarefa2/tarefa2.f90
@@ -1,38 +1,58 @@
 program DLA
     implicit none
-    integer, parameter :: N = 25
-    integer, parameter :: num_particles = 10000
+    integer, parameter :: N = 150
+    integer, parameter :: num_particles = 1000
+    integer, parameter :: pi = acos(-1.0d0)
     integer:: mesh(N,N), particles(2, num_particles), val(2)
-    integer:: i, j, cord, val_pos
-    real*8:: r, k
+    integer:: i, j, cord, val_pos, iterations
+    real*8:: r, k, max_radius, theta
     
+    open(unit=10, file="saida_1_13862330.out")
+
     mesh = 0
-    val = (-1, 1)
+    val = [-1, 1]
 
-    do i = 1, num_particles
-        particles(1, i) = N / 2
-        particles(2, i) = N / 2
+    particles(1, 1) = N / 2
+    particles(2, 1) = N / 2
 
-        mesh(particles(1, i), particles(2, i)) = 1
-    end do
+    max_radius = 0.0d0
 
-    do i = 1, num_particles
+    mesh(particles(1, 1), particles(2, 1)) = 1
+
+    do i = 2, num_particles
+        call ShowMesh(mesh, N)
+
+        max_radius = max(max_radius, ((particles(1, i-1)-N/2.0d0)**2.0d0 + (particles(2, i-1)-N/2.0d0)**2.0d0)**(1.0d0/2.0d0))
+
         call random_number(r)
         call random_number(k)
 
-        cord = int(2 * r) + 1
-        val_pos = int(2 * k) + 1
+        theta = 2.0d0 * pi * r
 
-        particles(cord, i) = particles(cord, i) + val(val_pos)
-        write(*,*)particles(cord, i), val(val_pos)
+        particles(1, i) = int(max_radius * cos(theta)) + 1
+        particles(2, i) = int(max_radius * sin(theta)) + 1
+
+        write(*,*)max_radius, particles(1, i), particles(2, i)
+
+        do while (.not.neighbor(particles(1, i), particles(2, i), mesh))
+            call random_number(r)
+            call random_number(k)
+
+            cord = int(2 * r) + 1
+            val_pos = int(2 * k) + 1
+
+            particles(cord, i) = particles(cord, i) + val(val_pos)
+
+            if (particles(1, i)**2.0d0 + particles(2, i)**2.0d0 .GT. 1.5d0*max_radius**2.0d0) then
+                particles(1, i) = int(max_radius * cos(theta)) + 1
+                particles(2, i) = int(max_radius * sin(theta)) + 1
+            end if
+        end do
 
         mesh(particles(1, i), particles(2, i)) = 1
-
-        call ShowMesh(mesh, N)
     end do
 
-
-contains
+contains 
 
 subroutine ShowMesh(mesh, N)
     implicit none
@@ -41,10 +61,28 @@ subroutine ShowMesh(mesh, N)
 
     do i = 1, N
         do j = 1, N
-            write(*,'(I4)', advance='no') mesh(i,j)
+            write(10,'(I4)', advance='no') mesh(i,j)
         end do
-        write(*,*)
+        write(10,*)
     end do
 end subroutine ShowMesh
 
+function neighbor(x, y, mesh) result(has_neighbor)
+    implicit none
+    integer:: x, y
+    integer:: mesh(N,N)
+    integer:: i, j
+    logical:: has_neighbor
+
+    has_neighbor = .false.
+
+    do i = -1, 1
+        do j = -1, 1
+            if (mesh(x+i,y+j).EQ.1) then
+                has_neighbor = .true.
+                return
+            end if
+        end do
+    end do
+end function neighbor
 end program DLA
